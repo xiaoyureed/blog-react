fb68894d771a022dfed8cfda336d190c0b480555
xiaoyu <775000738@qq.com>
2020-01-04 21:15:10 +0800

    Updates

---
+++ precache-manifest.81549cc0d0d676c4daae50dad54697f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca3bb7d1faca2a98af8e5ac228059db5",
+    "url": "/blog-react/index.html"
+  },
+  {
+    "revision": "06363da3498cfdafed79",
+    "url": "/blog-react/static/css/main.8e05c292.chunk.css"
+  },
+  {
+    "revision": "e58b88e30c27f7eba342",
+    "url": "/blog-react/static/js/2.252c9cb2.chunk.js"
+  },
+  {
+    "revision": "06363da3498cfdafed79",
+    "url": "/blog-react/static/js/main.bbeb2b4e.chunk.js"
+  },
+  {
+    "revision": "8510f38df9003cd1c5fc",
+    "url": "/blog-react/static/js/runtime~main.7d3294be.js"
+  },
+  {
+    "revision": "5cae69e3c394d5c040132161da824aaa",
+    "url": "/blog-react/static/media/avatar.5cae69e3.jpeg"
+  },
+  {
+    "revision": "fd0a5a14c9367ebc4390a4693aa634fa",
+    "url": "/blog-react/static/media/head.fd0a5a14.png"
+  }
+]);
